752030ef8f4c6c3d3922c16a7f3c176b5824c569
Matias <matimurias14@gmail.com>
2024-11-23 11:11:16 -0300

    quiosco-next-work

---
+++ app/layout.tsx
@@ -0,0 +1,21 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Quiosco Next.js con App Router y Prisma",
+  description: "Quiosco Next.js con App Router y Prisma",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className='bg-gray-100'>
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,6 @@
+
+export default function Home() {
+  return (
+    <h1>Hola Mundo desde Next.js</h1>
+  );
+}
